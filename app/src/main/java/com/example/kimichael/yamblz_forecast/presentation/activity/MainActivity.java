f99917a718d80da2d4de03f662b2e72d31cdf29d
kimichael <mikim123123123123@gmail.com>
2017-07-09 23:46:55 +0300

    Add saving selected fragment

--- app/src/main/java/com/example/kimichael/yamblz_forecast/presentation/activity/MainActivity.java
+++ app/src/main/java/com/example/kimichael/yamblz_forecast/presentation/activity/MainActivity.java
@@ -1,10 +1,8 @@
 package com.example.kimichael.yamblz_forecast.presentation.activity;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.IntDef;
 import android.support.v4.app.Fragment;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -19,10 +17,36 @@ import com.example.kimichael.yamblz_forecast.presentation.fragment.AboutFragment
 import com.example.kimichael.yamblz_forecast.presentation.fragment.SettingsFragment;
 import com.example.kimichael.yamblz_forecast.presentation.fragment.WeatherFragment;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    @Retention(RetentionPolicy.SOURCE)
+    @IntDef({FRAGMENT_STATUS_WEATHER, FRAGMENT_STATUS_SETTINGS, FRAGMENT_STATUS_ABOUT})
+    public @interface ChosenFragmentStatus {}
+    public static final int FRAGMENT_STATUS_WEATHER = 0;
+    public static final int FRAGMENT_STATUS_SETTINGS = 1;
+    public static final int FRAGMENT_STATUS_ABOUT = 2;
+
+    private @MainActivity.ChosenFragmentStatus
+    int mChosenFragment;
+
     @Override
+    protected void onResume() {
+        super.onResume();
+        changeFragment(mChosenFragment);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt(getString(R.string.key_chosen_fragment), mChosenFragment);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    @SuppressWarnings("ResourceType")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -38,8 +62,10 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.content_container, WeatherFragment.newInstance()).commit();
+        if (savedInstanceState != null)
+            changeFragment(savedInstanceState.getInt(getString(R.string.key_chosen_fragment), FRAGMENT_STATUS_WEATHER));
+        else
+            changeFragment(FRAGMENT_STATUS_WEATHER);
     }
 
     @Override
@@ -72,24 +98,42 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         int id = item.getItemId();
-        Fragment fragment;
 
         switch (id) {
             case R.id.nav_settings:
-                fragment = SettingsFragment.newInstance();
+                changeFragment(FRAGMENT_STATUS_SETTINGS);
                 break;
             case R.id.nav_about:
-                fragment = AboutFragment.newInstance();
+                changeFragment(FRAGMENT_STATUS_ABOUT);
                 break;
             case R.id.nav_weather:
+            default:
+                changeFragment(FRAGMENT_STATUS_WEATHER);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void changeFragment(@ChosenFragmentStatus int chosenFragment) {
+
+        Fragment fragment;
+        mChosenFragment = chosenFragment;
+
+        switch (chosenFragment) {
+            case FRAGMENT_STATUS_SETTINGS:
+                fragment = SettingsFragment.newInstance();
+                break;
+            case FRAGMENT_STATUS_ABOUT:
+                fragment = AboutFragment.newInstance();
+                break;
+            case FRAGMENT_STATUS_WEATHER:
             default:
                 fragment = WeatherFragment.newInstance();
         }
 
         getSupportFragmentManager().beginTransaction().replace(R.id.content_container, fragment).commit();
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
