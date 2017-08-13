892d1a2e5894f76f693a2607b03cb1c2671337d6
Sinjvf <sinjvf@gmail.com>
2017-08-13 14:10:20 +0300

    homework #4 -another one  deleting idea from git

--- app/src/test/java/com/example/kimichael/yamblz_forecast/views/TabletWeatherViewTest.java
+++ app/src/test/java/com/example/kimichael/yamblz_forecast/views/TabletWeatherViewTest.java
@@ -1,11 +1,13 @@
-package com.example.kimichael.yamblz_forecast.presenters;
+package com.example.kimichael.yamblz_forecast.views;
+
+import android.support.v7.widget.LinearLayoutManager;
 
 import com.example.kimichael.yamblz_forecast.data.common.PlaceData;
-import com.example.kimichael.yamblz_forecast.data.network.places.response.Prediction;
 import com.example.kimichael.yamblz_forecast.domain.interactor.settings.SettingsInteractor;
 import com.example.kimichael.yamblz_forecast.presentation.presenter.forecast.PhoneWeatherPresenter;
-import com.example.kimichael.yamblz_forecast.presentation.presenter.settings.SuggestsPresenter;
+import com.example.kimichael.yamblz_forecast.presentation.presenter.forecast.TabletWeatherPresenter;
 import com.example.kimichael.yamblz_forecast.presentation.view.main.phone.MainWeatherView;
+import com.example.kimichael.yamblz_forecast.presentation.view.main.tablet.TabletWeatherFragment;
 import com.example.kimichael.yamblz_forecast.presentation.view.places.SuggestsFragment;
 
 import org.junit.Before;
@@ -19,9 +21,7 @@ import org.mockito.runners.MockitoJUnitRunner;
 import java.util.ArrayList;
 import java.util.List;
 
-import io.reactivex.Observable;
 import io.reactivex.Scheduler;
-import io.reactivex.Single;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.verify;
@@ -32,9 +32,11 @@ import static org.mockito.Mockito.verify;
  */
 
 @RunWith(MockitoJUnitRunner.class)
-public class PhoneWeatherInteractorTest {
+public class TabletWeatherViewTest {
+    @Mock
+    private TabletWeatherFragment view;
 
-    private PhoneWeatherPresenter<MainWeatherView> presenter;
+    private TabletWeatherPresenter presenter;
     private SettingsInteractor interactor;
     private Scheduler scheduler;
 
@@ -43,14 +45,36 @@ public class PhoneWeatherInteractorTest {
         MockitoAnnotations.initMocks(this);
         this.interactor = Mockito.mock(SettingsInteractor.class);
         this.scheduler = Mockito.mock(Scheduler.class);
-        presenter = new PhoneWeatherPresenter<MainWeatherView>(interactor,scheduler);
-
+        presenter = new TabletWeatherPresenter(interactor,scheduler);
+        presenter.onAttach(view);
+        presenter.setCurrentList(new ArrayList<>());
     }
 
     @Test
-    public void checkGetCities() {
-        presenter.getCities();
-        verify(interactor).getAllCities(any());
+    public void checkAddCity() {
+        presenter.setItemPos(0);
+        verify(view).changeFragment(1, null);
+    }
+    @Test
+    public void checkSettings() {
+        presenter.setItemPos(1);
+        verify(view).changeFragment(2, null);
+    }
+
+    @Test
+    public void checkAbout() {
+        presenter.setItemPos(2);
+        verify(view).changeFragment(3, null);
+    }
+
+    @Test
+    public void checkToCity() {
+        List<PlaceData> list = new ArrayList<>();
+        PlaceData data = new PlaceData(4, "name", 4, 5);
+        list.add(data);
+        presenter.setCurrentList(list);
+        presenter.setItemPos(0);
+        verify(view).changeFragment(0, data);
     }
 
 }
